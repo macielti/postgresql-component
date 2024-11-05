43cab629e30a4054662b37eedbd781f6b66d9835
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-05 05:46:02 -0300

    externalise migrations tool

--- project.clj
+++ project.clj
@@ -7,24 +7,26 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.4.9"]
+  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.4.13"]
             [com.github.liquidz/antq "RELEASE"]
             [lein-shell "0.5.0"]]
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.pedestal/pedestal.interceptor "0.7.0"]
-                 [com.github.igrishaev/pg2-migration "0.1.18"]
-                 [com.github.igrishaev/pg2-core "0.1.18"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [io.pedestal/pedestal.interceptor "0.7.2"]
+                 [com.github.igrishaev/pg2-migration "0.1.19"]
+                 [com.github.igrishaev/pg2-core "0.1.19"]
                  [nubank/matcher-combinators "3.9.1"]
                  [com.taoensso/timbre "6.6.1"]
                  [clojure.java-time "1.4.2"]
                  [prismatic/schema "1.4.1"]
-                 [integrant "0.11.0"]
+                 [integrant "0.13.1"]
                  [hashp "0.2.2"]]
 
   :profiles {:dev {:resource-paths ^:replace ["test/resources"]
 
                    :test-paths     ^:replace ["test/unit" "test/integration" "test/helpers"]
+
+                   :dependencies   [[com.github.igrishaev/pg2-migration "0.1.18"]]
 
                    :injections     [(require 'hashp.core)]
 
@@ -35,15 +37,12 @@
                                     "clean-ns-fix"     ["clojure-lsp" "clean-ns"]
                                     "format-fix"       ["clojure-lsp" "format"]
                                     "lint-fix"         ["do" ["clean-ns-fix"] ["format-fix"]]
-                                    "outdated"         ["with-profile" "antq" "run" "-m" "antq.core"]
+                                    "migrate-all"      ["run" "-m" "pg.migration.cli" "-c" "resources/migration.config.edn" "migrate" "--all"]
                                     "auto-test"        ["do"
                                                         ["shell" "docker-compose" "-f" "test/resources/docker-compose.yml" "up" "-d"]
+                                                        ["migrate-all"]
                                                         ["test"]
-                                                        ["shell" "docker-compose" "-f" "test/resources/docker-compose.yml" "down"]]
-                                    "auto-test-remote" ["do"
-                                                        ["shell" "docker" "compose" "-f" "test/resources/docker-compose.yml" "up" "-d"]
-                                                        ["test"]
-                                                        ["shell" "docker" "compose" "-f" "test/resources/docker-compose.yml" "down"]]}
+                                                        ["shell" "docker-compose" "-f" "test/resources/docker-compose.yml" "down"]]}
                    :repl-options   {:init-ns postgresql-component.core}}}
 
   :repl-options {:init-ns postgresql-component.core}
